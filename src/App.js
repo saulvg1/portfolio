88685a900c7070fd5cab82cdb29ce636d163317b
Saul <saul.vg1@gmail.com>
2021-01-20 23:49:28 -0700

    added styles to each page and navbar. Add sticky scroll to top button. Imported fonts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 function App() {
   return (
     <div className="App">
+      <div className="scrollButton">
+        <a href="#about">Top</a>
+      </div>
       <nav id="nav">
         <div className="name">Saul Vega</div>
         <div className="links">
           <ul>
             <a href="#about">About</a>
-            <a href="#skills">Skills</a>
             <a href="#projects">projects</a>
-            <a href="#footer">Contact</a>
+            <a href="#contact">Contact</a>
           </ul>
         </div>
       </nav>
@@ -16,9 +18,9 @@ function App() {
         <a href="#about">Return To top</a>
       </div> */}
       <section id="about">This is the about section</section>
-      <section id="skills">This is the skills section</section>
       <section id="projects">This is the projects section</section>
-      <footer id="footer"> this is the footer </footer>
+      <section id="contact"> this is the </section>
+      <footer className="footer">This is the footer</footer>
     </div>
   );
 }
